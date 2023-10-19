402b68cf0e9cd0f42ff852583a0796749d5cfbc9
Greg <kimanthigregory>
2023-10-19 19:49:08 +0300

    worked on the responsiveness and cleaned up the jaavascript code

--- index.js
+++ index.js
@@ -2,57 +2,30 @@ const errorIcon = document.querySelector(".error-icon")
 const errorMessage = document.querySelector(".error-message");
 const emailInput = document.querySelector("#email");
 const outline = document.querySelector("input[type=email]:focus");
-
+// regular expression 
 const emailRegex =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
- 
-const emailValid = emailInput.value.length === 0 ||emailRegex.test(emailInput.value);
-// check if the email input is valid upon the load loading
-window.addEventListener("load", ()=>{
-    if (emailValid){
-        errorIcon.classList.remove("show-error-icon");
-        errorMessage.classList.remove("show-error-message");
-        errorIcon.classList.remove("show-error-outline");
-
-    }
-    else{
-        errorIcon.classList.add("show-error-icon");
-        errorMessage.classList.add("show-error-message");
-        errorIcon.classList.add("show-error-outline");
-    }
-})
-
-emailInput.addEventListener("input", ()=>{
+//  A function that does the the email validation using the reglar expression
+function emailValidation(){
+    
     const emailValid = emailInput.value.length === 0 ||emailRegex.test(emailInput.value);
 
     if (emailValid){
         errorIcon.classList.remove("show-error-icon");
         errorMessage.classList.remove("show-error-message");
         errorIcon.classList.remove("show-error-outline");
-        // alert("valid");
     }
     else{
         errorIcon.classList.add("show-error-icon");
         errorMessage.classList.add("show-error-message");
         errorIcon.classList.add("show-error-outline");
-        // alert("invalid");
     }
+
+}
+
+emailInput.addEventListener("input", ()=>{
+    emailValidation();
 })
 emailInput.addEventListener("submit",(event)=>{
     event.preventDefault();
-
-    const emailValid = emailInput.value.length === 0 ||emailRegex.test(emailInput.value);
-
-    if (emailValid){
-        errorIcon.classList.remove("show-error-icon");
-        errorMessage.classList.remove("show-error-message");
-        errorIcon.classList.remove("show-error-outline");
-        // alert("valid");
-    }
-    else{
-        errorIcon.classList.add("show-error-icon");
-        errorMessage.classList.add("show-error-message");
-        errorIcon.classList.add("show-error-outline");
-        // alert("invalid");
-    }
-
+    emailValidation();
 })
